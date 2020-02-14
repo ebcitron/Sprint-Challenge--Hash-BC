72a4b2d9e699ed8f2fa31fd9b8f7030b2cd9275f
ebcitron <ebcitron@gmail.com>
2020-02-14 11:10:08 -0600

    First Hashing problem solved

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -13,11 +13,29 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
 
-    return None
+    for i in range(len(weights)):
+        hash_table_insert(ht, weights[i], i)
+    answer = None
 
+    for i in range(len(weights)):
+        theRest = limit - weights[i]
+        value = weights[i]
+        index = i
+        correct = hash_table_retrieve(ht, theRest)
+        if correct is not None:
+            #If there is an answer
+            if i > correct:
+                answer = (i, correct)
+            else:
+                answer = (correct, i)
+            return answer
+    return answer
 
 def print_answer(answer):
     if answer is not None:
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
+
+print(get_indices_of_item_weights([4,6,10,15,16],5,21))
