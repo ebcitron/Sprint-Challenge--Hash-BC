d4ad4c912365244fcfef0b1f4fd091438f9c4a5d
ebcitron <ebcitron@gmail.com>
2020-02-14 11:45:48 -0600

    Finished
    Working
    Fin

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -25,6 +25,8 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+    while valid_proof(last_proof, proof) is False:
+        proof +=1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,7 +42,17 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+
+    last_hash = hashlib.sha256(f'{last_hash}'.encode()).hexdigest()
+    new_hash = hashlib.sha256(f'{proof}'.encode()).hexdigest()
+
+
+    print(f"Last:  {last_hash} | New:  {new_hash}")
+
+    return last_hash[-6:] == new_hash[:6]
+
+
+
 
 
 if __name__ == '__main__':
